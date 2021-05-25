624a2967b2b02d1ecb292e6335c6e0b6e130456f
Enes Cerrahoğlu <enescerrahoglu1@gmail.com>
2021-05-25 16:13:24 +0300

    Update hrms.sql

--- hrms.sql
+++ hrms.sql
@@ -5,8 +5,8 @@ BEGIN;
 
 CREATE TABLE public.departments
 (
-    id integer NOT NULL,
-    name character varying NOT NULL,
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name character varying(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -16,13 +16,14 @@ CREATE TABLE public.employers
     company_name character varying(50) NOT NULL,
     website character varying(100) NOT NULL,
     phone_number character varying(10) NOT NULL,
+    verification_status boolean NOT NULL,
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE public.job_positions
 (
-    id integer NOT NULL,
-    name character varying(50) NOT NULL,
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name character varying(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -47,8 +48,8 @@ CREATE TABLE public.system_personnel
 
 CREATE TABLE public.users
 (
-    id integer NOT NULL,
-    email character varying(50) NOT NULL,
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 9 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    email character varying(250) NOT NULL,
     password character varying(250) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -66,7 +67,7 @@ ALTER TABLE public.job_seekers
 
 
 ALTER TABLE public.system_personnel
-    ADD FOREIGN KEY (user_id)
+    ADD FOREIGN KEY (department_id)
     REFERENCES public.departments (id)
     NOT VALID;
 
